44b9808535b0e724bee6e8cd998e17a0ce4de7ee
tanaphatr <161965098+tanaphatr@users.noreply.github.com>
2025-03-13 22:48:26 +0700

    ALL

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  poweredByHeader: false,
+  webpack(config: { devtool: boolean; }, { dev, isServer }: any) {
+    if (!dev && !isServer) {
+      // ปิดการสร้าง source maps ใน production
+      config.devtool = false;
+    }
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
